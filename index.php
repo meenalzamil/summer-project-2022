77360ec2795a3b465385b86f0e87fa1e429bcb7a
meenalzamil <meenalzamil@gmail.com>
2022-06-01 09:41:14 -0500

    getting it to work!

--- index.php
+++ index.php
@@ -10,11 +10,25 @@
             exit;
         }
         echo"
-        <div class='block'>
+        <div class='space'>
             <a class='linking' href='displayPosts.php?postID=$postID'><h2 class='blockTwoColor'>$post[title]</h2></a>
-            <div>$post[summary]</div>
+            <div class='aboutMe'>$post[summary]</div>
         </div>
         ";
+    }
+    closeBlock();
+    breaks();
+    blocks();
+    image($backgroundImage='backgroundThree');
+    foreach(getAllPosts() as $postID => $post){
+        if ($postID > 2){
+            echo"
+            <div class='space'>
+                <a class='linking' href='displayPosts.php?postID=$postID'><h2 class='blockTwoColor'>$post[title]</h2></a>
+                <div class='aboutMe'>$post[summary]</div>
+            </div>
+            ";
+        }
     }
     closeBlock();
     footer();
@@ -38,3 +52,25 @@
         $textTwo = 'There are so many experiences and people that introduced me to the things I love to do. Here is a little section on the moments that I think are important when discussing who I am. ', 
         $textThree = 'Here is a look into my work experience and the tasks I was repsonsible for.');
     breaks
+
+    function blocks($blockNumber, $backgroundImage, $row, $block, $linking, $href, $textTag, $color, $sectionOne, $sectionTwo, $sectionThree, $textOne, $textTwo, $textThree){
+    echo "<div id='".$blockNumber."'>
+    <div id='".$backgroundImage."'>
+    </div>
+        <div  class='".$row."'>
+            <div class='".$block."'>
+                <a class='".$linking."' href='$href'><h2 class='".$color."'>$sectionOne</h2></a>
+                <p class='".$textTag."'> $textOne </p>
+
+            </div>
+            <div class='".$block."'>
+                <a class='".$linking."' href='$href'><h2 class='".$color."'>$sectionTwo</h2></a>
+                <p class='".$textTag."'> $textTwo </p>
+            
+            </div>
+            <div class='".$block."'>
+                <a class='".$linking."' href='$href'><h2 class='".$color."'>$sectionThree</h2></a>
+                <p class='".$textTag."'>$textThree</p>
+            </div>
+        </div>
+</div>
