4bbef292ec9f3c07abfacb81054e4a69b7166261
meenalzamil <meenalzamil@gmail.com>
2022-06-07 10:51:51 -0500

    clearing out

--- include/initialize.php
+++ include/initialize.php
@@ -6,7 +6,6 @@ ini_set('display_errors', '1');
 include_once('connect.php');
 include_once('common_components.php');
 include_once('posts.php');
-include_once('helperFunctions.php');
 include_once('selectComments.php');
 
 
