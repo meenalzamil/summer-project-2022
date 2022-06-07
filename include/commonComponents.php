fb74d1a0f61b4e40c11e616c17a26d0abd701aa0
meenalzamil <meenalzamil@gmail.com>
2022-06-07 11:27:14 -0500

    sadgfd

--- include/commonComponents.php
+++ include/commonComponents.php
@@ -1,2 +1,20 @@
 <?php
 include_once('initialize.php');
+
+function echoHeader() {
+    echo "
+    <html>
+
+    <head>
+    <title>This is our title!</title>
+    <link rel='stylesheet' href='styles.css'>
+    <link rel='preconnect' href='https://fonts.googleapis.com'>
+    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+    <link href='https://fonts.googleapis.com/css2?family=Koulen&family=Roboto+Mono:wght@400;500&display=swap' rel='stylesheet'>
+    </head>
+    <body>
+    <div id='beginning'>
+    <p id='header'>M e e n a  &nbsp; Z a m i l</p>
+   </div>
+    ";
+}
