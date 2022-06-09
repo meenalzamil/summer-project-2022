efcdabbd5701a1802d079f43df42c37cf1b2f153
meenalzamil <meenalzamil@gmail.com>
2022-06-09 10:55:34 -0500

    final edits before pull req

--- include/functions.php
+++ include/functions.php
@@ -1,25 +1,20 @@
 <?php
 include_once('initialize.php');
 function createNewComment($name, $comment, $postId){
-    $successfulCommentInsertion = false;
+    global $pdo;
     if (!empty($name) && !empty($comment)){
-        $successfulCommentInsertion = dbQuery("
+            dbQuery("
             INSERT INTO comments (name, comment, postId)
             VALUES ('$name', '$comment', '$postId')
         ");
-    }
-    if ($successfulCommentInsertion){
-        return "success";
-    }
-    else{
-        return "error";
+        return $newId = $pdo->lastInsertId();
     }
 }
 
 function createNewPost($location, $content, $dateOfTravel, $tripId){
     global $pdo;
     if (!empty($location) && !empty($content) && !empty($dateOfTravel)){
-        $result = dbQuery("
+            dbQuery("
             INSERT INTO post (location, dateOfTravel, content, tripId)
             VALUES ('$location', '$dateOfTravel', '$content', '$tripId')
         ");
@@ -30,7 +25,7 @@ function createNewPost($location, $content, $dateOfTravel, $tripId){
 function createNewTrip($location, $blurb, $startDate, $endDate){
     global $pdo;
     if(!empty($location) && !empty($blurb) && !empty($startDate) && !empty($endDate)){
-        $result = dbQuery("
+            dbQuery("
             INSERT INTO trip (location, blurb, startDate, endDate)
             VALUES ('$location', '$blurb', '$startDate', '$endDate')
         ");
