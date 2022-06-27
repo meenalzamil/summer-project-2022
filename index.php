f9168ac33b11a6d2fe35e0ae304e6fcd68d71b03
meenalzamil <meenalzamil@gmail.com>
2022-06-27 11:47:05 -0500

    the first half of home page is working

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
 <?php
-
-include_once ('include/initialize.php');
+include_once('include/initialize.php');
 echoHeader();
-foreach(getAllTrips() as $index => $trip){
- 
-        echo"
-        <div >
-            <a style='color: #27676D' href='displayTrips.php?tripId=$trip[tripId]'>$trip[location]</a>
-            <div class='greenSection'>$trip[blurb]</div>
-        </div>
-        <br/>
-        ";
-}
-echo"
-<h3>Add a new trip!<h3>
-<form action='handleNewTrip.php' method='post'>
-    Blurb: <textarea rows='10' cols='30' name='blurb'></textarea><br>
-    Location: <input type='text' name = 'location'><br>
-    Start Date: <input type='date' name= 'startDate'><br>
-    End Date: <input type='date' name= 'endDate'><br>
-    <input type='submit' name='submitButton'>
-</form>
-";
+navBar();
+firstPageIntro();
 footer();
-
-
